399b94ec48f6b86fcc483a2be570e9cec47ad719
Florian Kinder <florian.kinder@fankserver.com>
2018-08-06 23:10:56 +0200

    Initial v2 creation

---
+++ Data/Scripts/NaniteConstructionSystem/Entities/ControlFacility.cs
@@ -0,0 +1,38 @@
+﻿using Sandbox.Common.ObjectBuilders;
+using Sandbox.ModAPI;
+using System;
+using VRage.Game.Components;
+using VRage.ModAPI;
+using VRage.ObjectBuilders;
+
+namespace Ntech.Nanite
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Welder), false, "LargeNaniteFactory")]
+    class LargeControlFacilityLogic : MyGameLogicComponent
+    {
+        public IMyShipWelder Welder => (IMyShipWelder)Entity;
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            try
+            {
+                base.Init(objectBuilder);
+
+                NeedsUpdate |= MyEntityUpdateEnum.EACH_FRAME;
+                NeedsUpdate |= MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
+            }
+            catch (Exception ex) { NaniteConstructionSystem.Logging.Instance.WriteLine($"Exception in EntityInit: {ex}"); }
+        }
+
+        public override void UpdateOnceBeforeFrame()
+        {
+            base.UpdateOnceBeforeFrame();
+            try
+            {
+                if (Welder.CubeGrid.Physics == null) return;
+                Session.Instance.LargeControlFacilityLogics.Add(this);
+            }
+            catch (Exception ex) { NaniteConstructionSystem.Logging.Instance.WriteLine($"Exception in UpdateOnceBeforeFrame: {ex}"); }
+        }
+    }
+}
